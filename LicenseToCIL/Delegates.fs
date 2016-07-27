f6e2fee0ac573a1b614b2aff05e347a8f6e629e1
Robert Peele <robert.s.peele@gmail.com>
2016-07-27 00:20:54 -0400

    Add delegate helper.

---
+++ LicenseToCIL/Delegates.fs
@@ -0,0 +1,18 @@
+﻿[<AutoOpen>]
+module LicenseToCIL.Delegates
+open LicenseToCIL.Stack
+open System
+open System.Reflection
+open System.Reflection.Emit
+
+let toDelegate<'del when 'del :> Delegate> name (op : Op<E, E>) =
+    let del = typeof<'del>
+    let invoke = del.GetMethod("Invoke")
+    let returnType = invoke.ReturnType
+    let parameterTypes = invoke.GetParameters() |> Array.map (fun p -> p.ParameterType)
+    let meth = DynamicMethod(name, returnType, parameterTypes)
+    ignore <| op Stack.empty (IL(meth.GetILGenerator()))
+    downcast meth.CreateDelegate(del)
+    : 'del
+
+
