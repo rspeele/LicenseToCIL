870bc5ae8a545d33f0bfd89de3cf91ef9bf345f8
Robert Peele <robert.s.peele@gmail.com>
2016-08-19 00:19:10 -0400

    Add my name to assemblyinfo copyright.

--- LicenseToCIL/AssemblyInfo.fs
+++ LicenseToCIL/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("LicenseToCIL")>]
-[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyCopyright("Copyright © Robert Peele 2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
