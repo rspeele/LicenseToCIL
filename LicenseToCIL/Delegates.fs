2cdf3f1d82abc7b3bd385236900f0d0e9cd2c87c
Robert Peele <robert.s.peele@gmail.com>
2016-08-19 00:23:09 -0400

    Add parameter name for clarity.

--- LicenseToCIL/Delegates.fs
+++ LicenseToCIL/Delegates.fs
@@ -10,9 +10,10 @@ let toDelegate<'del when 'del :> Delegate> name (op : Op<E, E>) =
     let invoke = del.GetMethod("Invoke")
     let returnType = invoke.ReturnType
     let parameterTypes = invoke.GetParameters() |> Array.map (fun p -> p.ParameterType)
-    let meth = DynamicMethod(name, returnType, parameterTypes, true)
+    let meth = DynamicMethod(name, returnType, parameterTypes, restrictedSkipVisibility = true)
     ignore <| op Stack.empty (IL(meth.GetILGenerator()))
     downcast meth.CreateDelegate(del)
     : 'del
+
 
 
