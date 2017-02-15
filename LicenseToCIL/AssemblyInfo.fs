6e679e4490b4b0b10cd48a7aeded1a4fec910601
Robert Peele <robert.s.peele@gmail.com>
2017-02-14 21:03:57 -0500

    Add strong name and bump package version.

--- LicenseToCIL/AssemblyInfo.fs
+++ LicenseToCIL/AssemblyInfo.fs
@@ -34,8 +34,12 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.2.1.*")>]
-[<assembly: AssemblyFileVersion("0.2.1.*")>]
+[<assembly: AssemblyVersion("0.2.2.*")>]
+[<assembly: AssemblyFileVersion("0.2.2.*")>]
+#if !DEBUG
+[<assembly: AssemblyKeyFile("../../robert.peele.snk")>]
+#endif
+
 
 do
     ()
